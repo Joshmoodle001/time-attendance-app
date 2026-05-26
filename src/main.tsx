d0bb58c90392071bdf6e10ffa7d644532fdc9246
joshmoodle001 <joshmoodley001@gmail.com>
2026-05-26 15:31:16 +0200

    Remove global sign-in shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AuthApp from './AuthApp.tsx'
+import App from './App.tsx'
 import ErrorBoundary from './components/ErrorBoundary.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <AuthApp />
+      <App />
     </ErrorBoundary>
   </StrictMode>,
 )
